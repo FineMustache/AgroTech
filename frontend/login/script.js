067b87761ca270721b20874fcb31c47dc9d31f47
FineMustache <guihenrique2806@gmail.com>
2023-02-23 11:18:36 -0300

    Página Login

---
+++ frontend/login/script.js
@@ -0,0 +1,25 @@
+function login() {
+    let email = document.querySelector('#email').value
+    let senha = document.querySelector('#senha').value
+
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: `{"email":"${email}","senha":"${senha}"}`
+      };
+      
+      fetch('http://localhost:3000/agrotech/login', options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.erro)
+            if (response.erro !== undefined) {
+                document.querySelector('.error').innerHTML = response.erro
+                document.querySelector('.error').classList.remove('escondido')
+            } else {
+                document.querySelector('.error').classList.add('escondido')
+                localStorage.setItem('@uinfo', JSON.stringify(response))
+                console.log('logou')
+            }
+        })
+        .catch(err => console.error(err));
+}
